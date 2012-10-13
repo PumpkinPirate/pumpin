06ef52e830bf39c74641c2e44196cfaf778ca037
Andrew Stoneman <andrew@pumpkinpirate.info>
2012-10-12 18:11:30 -0700

    More Skeleton

--- pumpin/settings.py
+++ pumpin/settings.py
@@ -80,10 +80,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    
+    'pumpin.gallery',
 )
 
 # A sample logging configuration. The only tangible logging
--- pumpin/urls.py
+++ pumpin/urls.py
@@ -5,9 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pumpin.views.home', name='home'),
-    # url(r'^pumpin/', include('pumpin.foo.urls')),
+    url(r'^', include('pumpin.gallery.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
