6fbd721146cc7777c4c437db9036a97604265384
Andrew Stoneman <andrew@pumpkinpirate.info>
2012-10-12 17:46:53 -0700

    Project skeleton

---
+++ pumpin/settings_local-base.py
@@ -0,0 +1,46 @@
+# Local django settings for this installation of sr project.
+# Copy this to localSettings.py, and then customize as appropriate.
+# Do not 'svn add' localSettings.py.
+
+from os.path import join
+
+siteDir = '/path/to/pumpin'
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': join(siteDir, 'pumpin.db'),     # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'US/Pacific'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = join(siteDir, 'static')
+
+TEMPLATE_DIRS = (
+    join(siteDir, 'templates/gullible'),
+    #join(siteDir, 'templates/nampv'),
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '$1%68n_!0%w^dxl(do*f*mwu$$9-e-uorr0ii_$^o!h2%uyqmi'
